7ea1207650082167921a240a00316ba3d0723866
Konic2 <ursandragos@yahoo.com.sg>
2023-04-28 19:44:53 +0300

    Finally, the animation for attack works as intended

--- animator.gd
+++ animator.gd
@@ -1,7 +1,7 @@
 extends AnimationTree
 @onready var animState:AnimationNodeStateMachinePlayback = get("parameters/playback")
 var currentAnimState: String = ""
-var started: bool = false
+var started:bool = false
 var finished: bool = true
 
 # Transform them into enums could also work
@@ -10,20 +10,32 @@ var allAnimationStates = [\
 	"Idle", "Walk", #interruptable
 	"Throw", "Ranged", "Melee", "Magic", #non-interruptable
 	"Hit", "Death"] #non-interruptable
-
+var interruptableAnimations = ["Idle", "Walk"]
 func playAnimation(animName:String,animDir:Vector2):
 	var animationName = ""
 	for animation in allAnimationStates:
 			animationName = animName
 			break
 	if animationName != "":
-		if currentAnimState !=  animName:
-			print(animName)
+		#if animName can be interrupted
+		#i can choose any other animation
+		#this also means that i should not replay this animation
+		#also, i need to make sure something else did not start
+		if animName in interruptableAnimations and currentAnimState != animName and !started:
 			animState.stop()
 			animState.travel(animName)
 			animState.start(animName)
 			currentAnimState =  animName
 			set("parameters/" + animName + "/blend_position",animDir)
+		#if animName cannot be interrupted
+		#i can only change when it finished
+		if animName not in interruptableAnimations and finished:
+			animState.stop()
+			animState.travel(animName)
+			animState.start(animName)
+			currentAnimState =  animName
+			set("parameters/" + animName + "/blend_position",animDir)
+			started = true
 		else:
 			set("parameters/" + currentAnimState + "/blend_position",animDir)
 				

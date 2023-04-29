0fe85b65e515b84826de50c3bd45f9ce82c10f9b
Konic2 <ursandragos@yahoo.com.sg>
2023-04-29 18:39:19 +0300

    Added ability to pick up items (including chickens !!) Currently can't put items down, need to add timer for that.(and timer for attacks)

--- Scripts/Actor.gd
+++ Scripts/Actor.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var speed :float = 100
 @export var moveDir : Vector2 = Vector2(0,0.1)
 @export var faceDir = "res://Assets/Puny-Characters/"
+@onready var itemPos = $ItemPosition
 
 #player specific, currently never resolves to find RemoteTransfrom2D for BadActors
 @onready var remTran = $RemoteTransform2D
@@ -22,6 +23,13 @@ var blackboard : Dictionary= {}
 var dead = false
 var lastDir = Vector2.ZERO
 var attacked = false
+#item pickups
+var itemClose = false
+var itemOldParent = null
+var item:Node = null
+var hasItem = false
+#use timers or something like that idk xd
+#var actionDelay = 0.5
 
 func death():
 	animationTree.playAnimation("Dead",moveDir)
@@ -43,13 +51,43 @@ func _ready():
 		var camera2D = get_parent().get_node("camera").get_path()
 		remTran.set_remote_node(camera2D)
 	#blackboard["target"] = groupMembers[0]
+
+
+
+func itemPickUp():
+	if itemClose:
+		item.reparent(self)
+		item.position = itemPos.position 
+		#item.scale = item.scale
+		var coll:CollisionShape2D = item.find_child("CollisionBox")
+		if coll:
+			coll.disabled = true
+		hasItem = true
+func itemUnPick():
+	if hasItem:
+		print("bye")
+		item.reparent(get_parent())
+		var coll:CollisionShape2D = item.find_child("CollisionBox")
+		if coll:
+			coll.disabled = false
+		hasItem = false
+		itemClose = false
+		
+func itemProcess():
+	#follow direction on the horziontal :D
+	pass
 func player_process(delta):
 	#take advantage of the fact that the values are clamped between [-1,1]
 	var horizontal = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	var vertical = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	moveDir = Vector2(horizontal,vertical)
-	if Input.get_action_strength("Attack"):
+	if Input.get_action_raw_strength("Attack"):
 		animationTree.playAnimation("Melee",moveDir)
+		#need some sort of timer for PickUp and UnPick
+		if !hasItem:
+			itemPickUp()
+		#else:
+		#	itemPickUp()
 func ai_process(delta):
 	blackboard["speed"] = speed
 	# xd
@@ -81,12 +119,28 @@ func _physics_process(delta):
 
 
 
-
+#extend the hitbox through animation :D
 func UponHurt(area):
-	if area not in get_children() and area.name == "HurtArea":
+	if area not in get_children() and area.name == "HitArea":
 		attacked = true
 
 
 func UponSafe(area):
-	if area not in get_children() and area.name == "HurtArea":
+	if area not in get_children() and area.name == "HitArea":
 		attacked = false
+
+
+func ItemClose(area):
+	if area not in get_children() and area.name.begins_with("Item"):
+		print("We are rich ",area.name)
+		itemClose = true
+		item = area.get_parent()
+		itemOldParent = item.get_parent()
+
+
+func ItemFar(area):
+	if area not in get_children() and area.name.begins_with("Item"):
+		print("We are poor ",area.name)
+		itemClose = false
+		item = null
+		itemOldParent = null

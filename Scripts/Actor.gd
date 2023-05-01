13a5c1c02d871367012298f7b9e5ec0abb21697b
Konic2 <ursandragos@yahoo.com.sg>
2023-05-01 16:59:08 +0300

    A lot of things happened, I might drop the behaviour trees in order to make it easier to implement the finite state machine. Next commit should be with a refactored version of Decider.gd

--- Scripts/Actor.gd
+++ Scripts/Actor.gd
@@ -19,10 +19,16 @@ extends CharacterBody2D
 @onready var groupMembers = get_tree().get_nodes_in_group("targetable")
 var blackboard : Dictionary= {}
 
+var itemGoal = ["ItemChicken","ItemBottle","ItemEdible","ItemMetal","ItemGem","ItemMaterial"]
+var goal:String = itemGoal[randi()%itemGoal.size()]
 #Additional mechanics
 var dead = false
 var lastDir = Vector2.ZERO
 var attacked = false
+var health:float = 100.0
+var canBeAfraid = false
+var afraid = false
+
 #item pickups
 var itemClose = false
 var itemOldParent = null
@@ -33,6 +39,11 @@ var hasItem = false
 
 func death():
 	animationTree.playAnimation("Dead",moveDir)
+	$HurtArea.monitorable = false
+	$HurtArea.monitoring = false
+	$CollisionBox.disabled = true
+	$HitArea.monitorable = false
+	$HitArea.monitoring = false
 	dead = true
 func randomModel():
 	var dir = DirAccess.open(faceDir)
@@ -46,13 +57,18 @@ func randomModel():
 					break
 			file = dir.get_next()
 func _ready():
+	blackboard["action_Pick"] = Callable(self,"itemPickUp")
+	blackboard["action_Drop"] = Callable(self,"itemUnPick")
+	blackboard["action_Death"] = Callable(self,"death")
+	blackboard["action_ChangeModel"] = Callable(self,"randomModel")
 	randomModel()
 	if player:
 		var camera2D = get_parent().get_node("camera").get_path()
 		remTran.set_remote_node(camera2D)
+	else:
+		if randi_range(0,10) >= 5:
+			canBeAfraid = true
 	#blackboard["target"] = groupMembers[0]
-
-
 
 func itemPickUp():
 	if itemClose:
@@ -63,43 +79,55 @@ func itemPickUp():
 		if coll:
 			coll.disabled = true
 		hasItem = true
+
 func itemUnPick():
 	if hasItem:
 		print("bye")
+		item.position += Vector2(randf_range(0,1.0),randf_range(0,1.0))
 		item.reparent(get_parent())
 		var coll:CollisionShape2D = item.find_child("CollisionBox")
-		if coll:
-			coll.disabled = false
+		#if coll:
+		#	coll.disabled = false
 		hasItem = false
 		itemClose = false
-		
+
 func itemProcess():
 	#follow direction on the horziontal :D
 	pass
+
+
 func player_process(delta):
 	#take advantage of the fact that the values are clamped between [-1,1]
 	var horizontal = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	var vertical = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	moveDir = Vector2(horizontal,vertical)
 	if Input.get_action_raw_strength("Attack"):
-		animationTree.playAnimation("Melee",moveDir)
-		#need some sort of timer for PickUp and UnPick
+		#you cannot attack whilst holding an item
 		if !hasItem:
+			animationTree.playAnimation("Melee",moveDir)
+			#need some sort of timer for PickUp and UnPick
 			itemPickUp()
-		#else:
-		#	itemPickUp()
+	if Input.get_action_raw_strength("Alternative"):
+		if hasItem:
+			itemUnPick()
+
+
 func ai_process(delta):
 	blackboard["speed"] = speed
-	# xd
-	#var debugPlayer = get_parent().get_node("Player")
-	#blackboard["target"] = debugPlayer
-	
+	blackboard["delta"] = delta
+	blackboard["attacked"] = attacked
+	blackboard["itemClose"] = itemClose
+	blackboard["hasItem"] = hasItem
+	blackboard["canBeAfraid"] = canBeAfraid
+	blackboard["afraid"] = afraid
 	decider.think(self,blackboard)
 	
 func no_process(delta):
 	pass
 	
 func _physics_process(delta):
+	if health <=0:
+		death()
 	if not dead:
 		if player:
 			player_process(delta)
@@ -117,30 +145,36 @@ func _physics_process(delta):
 		if velocity != Vector2.ZERO:
 			move_and_slide()
 
-
-
-#extend the hitbox through animation :D
 func UponHurt(area):
 	if area not in get_children() and area.name == "HitArea":
+		health -= randf_range(10.0,20.0)
+		print(health)
 		attacked = true
+		if canBeAfraid:
+			afraid = true
 
 
 func UponSafe(area):
 	if area not in get_children() and area.name == "HitArea":
 		attacked = false
+		if canBeAfraid:
+			afraid = false
 
 
 func ItemClose(area):
 	if area not in get_children() and area.name.begins_with("Item"):
-		print("We are rich ",area.name)
-		itemClose = true
-		item = area.get_parent()
-		itemOldParent = item.get_parent()
+		#print("We are rich ",area.name)
+		#players can pick all items, npc's can only pick specific items
+		if area.name == goal or player:
+			itemClose = true
+			item = area.get_parent()
+			itemOldParent = item.get_parent()
 
 
 func ItemFar(area):
 	if area not in get_children() and area.name.begins_with("Item"):
-		print("We are poor ",area.name)
-		itemClose = false
-		item = null
-		itemOldParent = null
+		#print("We are poor ",area.name)
+		if area.name == goal:
+			itemClose = false
+			item = null
+			itemOldParent = null
